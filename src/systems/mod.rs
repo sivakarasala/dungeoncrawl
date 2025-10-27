b565f3043c9293f80967a55f248a06a92b8c8e76
Siva Karasala <shivataraya@gmail.com>
2025-10-27 16:08:10 +0530

    feat: add fov

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,6 +2,7 @@ mod chasing;
 mod combat;
 mod end_turn;
 mod entity_render;
+mod fov;
 mod hud;
 mod map_render;
 mod movement;
@@ -14,6 +15,7 @@ use crate::prelude::*;
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(fov::fov_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
@@ -28,6 +30,7 @@ pub fn build_player_scheduler() -> Schedule {
         .flush()
         .add_system(movement::movement_system())
         .flush()
+        .add_system(fov::fov_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
@@ -44,6 +47,8 @@ pub fn build_monster_scheduler() -> Schedule {
         .add_system(combat::combat_system())
         .flush()
         .add_system(movement::movement_system())
+        .flush()
+        .add_system(fov::fov_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
